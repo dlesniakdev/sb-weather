37fd09f5c0d8f4a6f5d8de4577d42c59fef011bf
bbZ <gamemajster@gmail.com>
2018-08-03 15:54:32 +0200

    Error handling

--- src/content/chart/Charts.js
+++ src/content/chart/Charts.js
@@ -11,18 +11,19 @@ class Charts extends Component {
     this.state = {
       forecasts: [],
       isLoading: false,
+      isError: false,
       name: name
     };
   }
 
   prepareRequest = () => {
-    const { name } = this.state.name
+    const { name } = this.state.name;
 
     var headers = new Headers();
     headers.append("Authorization", "Basic dXNlcjp1c2Vy");
 
     return ({
-      uri: encodeURI('http://sb-weather-api.eu-central-1.elasticbeanstalk.com/api/sbforecast?city=' + name),
+      uri: encodeURI('http://localhost:8080/api/sbforecast?city=a' + name),
       headers: headers
     })
   }
@@ -32,8 +33,11 @@ class Charts extends Component {
     const { name } = this.state.name
 
     var requestTemlate = this.prepareRequest()
-    fetch(requestTemlate.uri, {headers: requestTemlate.headers})
-      .then(result => result.json())
+    fetch(requestTemlate.uri, { headers: requestTemlate.headers })
+      .then(response => {
+        if (!response.ok) { throw response }
+        return response.json()
+      })
       .then(json => json.forecasts)
       .then(forecasts => gatherData(forecasts))
       .then(data => {
@@ -41,21 +45,30 @@ class Charts extends Component {
           <div>
             <h1 className='chart-title'>Forecast charts for {name}</h1>
             <ReactEcharts option={temperatureChart(data)} />
-            <div className='mt4'/>
+            <div className='mt4' />
             <ReactEcharts option={pressureChart(data)} />
-            <div className='mt4'/>
+            <div className='mt4' />
             <ReactEcharts option={humidityChart(data)} />
           </div>
         )
       })
       .then(forecasts => this.setState({ forecasts: forecasts, isLoading: false }))
+      .catch(err =>
+        err.text().then(errorMessage => {
+          this.setState({ isError: true, isLoading: false });
+          console.error(err)
+        })
+      )
+
   }
 
   render() {
-    const { isLoading } = this.state;
+    const { isLoading, isError } = this.state;
 
     if (isLoading) {
       return <p>Loading ...</p>;
+    } else if (isError) {
+      return <p>Error occurred, please try again later.</p>
     }
 
     return (
